65dac86fcc402842f0d183c0203a22db137e798c
shubham-tyagi <raj.tyagi2000@gmail.com>
2021-09-16 22:37:11 +0530

    DRY methodology implemented, code refactoring done

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
-// document.querySelector('.number').textContent = secretNumber;
 
+// To change the text of a particular HTML element
+const changeTxtContent = function (identifier, txt) {
+  document.querySelector(identifier).textContent = txt;
+};
+
+// To change the css property of certain HTML element.
+const editCss = function (identifier, property, value) {
+  document.querySelector(identifier).style[property] = value;
+};
+
+// reseting the whole page if user wants to play again
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.check').style.display = 'inline-block';
-  document.querySelector('body').style.backgroundColor = '#222';
+  changeTxtContent('.message', 'Start guessing...');
+  document.querySelector('.check').style['display'] = 'inline-block';
+  editCss('body', 'backgroundColor', '#222');
   document.querySelector('.guess').value = '';
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').textContent = '?';
+  editCss('.number', 'width', '15rem');
+  changeTxtContent('.number', '?');
   document.querySelector('.score').textContent = score;
-  //   document.querySelector('.number').textContent = secretNumber;
+  changeTxtContent('.score', '?');
 });
 
+// check the input value entered by the user
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
-
-  if (guess > 20 || guess < 1) {
-    document.querySelector('.message').textContent =
-      'Enter a number between 1 to 20';
+  // this if else covers all the edge cases that can be entered by the user
+  if (!guess) changeTxtContent('.message', 'No Number!');
+  else if (guess > 20 || guess < 1) {
+    changeTxtContent('.message', 'Enter a number between 1 and 20');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Answer';
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '25rem';
-    document.querySelector('.number').textContent = secretNumber;
-    this.style.display = 'none';
+    changeTxtContent('.message', 'Correct Answer');
+    editCss('body', 'backgroundColor', '#60b347');
+    editCss('.number', 'width', '25rem');
+    changeTxtContent('.number', secretNumber);
+    editCss('.check', 'display', 'none');
     highScore = Math.max(score, highScore);
-    document.querySelector('.highscore').textContent = highScore;
+    changeTxtContent('.highscore', highScore);
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High';
+    changeTxtContent('.message', 'Too High');
     score--;
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
+    changeTxtContent('.message', 'Too Low');
     score--;
-  } else if (!guess) {
-    document.querySelector('.message').textContent = 'Enter a number to guess';
   }
+  // change the value of score if user entered wrong valid value
   document.querySelector('.score').textContent = score;
+
+  // if the user has completed his 20 given chance then the user looses
   if (score == 0) {
-    document.querySelector(
-      '.message'
-    ).textContent = `Wrong answer\r\nYou are out of chances\r\nclick 'Again!' to try again`;
+    changeTxtContent('.message', 'Wrorng Answer');
     document.querySelector('body').style.backgroundColor = 'red';
-    this.style.display = 'none';
+    editCss('.check', 'display', 'none');
   }
 });
